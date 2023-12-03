dd9d48bd4aae694567fc5155c362766fe0c9f83c
DeveloperDave17 <DavidHennigan17@gmail.com>
2023-12-03 09:15:41 -0500

    add Junit Suite

---
+++ src/test/java/com/example/blackjack/BlackjackTestSuite.java
@@ -0,0 +1,9 @@
+package com.example.blackjack;
+
+import org.junit.platform.suite.api.SelectPackages;
+import org.junit.platform.suite.api.Suite;
+
+@Suite
+@SelectPackages({"com.example.blackjack"})
+public class BlackjackTestSuite {
+}
